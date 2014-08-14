daffeec60305605d107a17fc6e8b5295eb6ec645
Ce Zhang <czhang@Ces-MacBook-Pro.local>
2014-08-14 14:20:01 -0700

    julia

--- examples/julia_lr_scd.jl
+++ examples/julia_lr_scd.jl
@@ -40,20 +40,28 @@ function loss(row::Array{Cdouble,1}, model::Array{Cdouble,1})
 	return (-label * d + log(exp(d) + 1.0))
 end
 
-function grad(row::Array{Cdouble,1}, model::Array{Cdouble,1})
-	@inbounds begin
-		const label = row[length(row)]
-		const nfeat = length(model)
+function grad(col::Array{Cdouble,1}, _colid::Cint, rows::Array{Array{Cdouble, 1}}, model::Array{Cdouble,1})
+	colid = _colid + 1
+	nfeat = length(model)
+	nrows = length(rows)
+	if colid > nfeat	# if it is the last column, then it is the labels, do nothing
+		return 1.0
+	end
+
+	sum_term = 0.0
+	pat_term = 0.0
+	for ir = 1:length(rows)
+		label = rows[ir][nfeat+1]
 		d = 0.0
 		for i = 1:nfeat
-			d = row[i]*model[i]
+			d = d + rows[ir][i]*model[i]
 		end
-		d = exp(-d)
-  		Z = 0.00001 * (-label + 1.0/(1.0+d))
-	  	for i = 1:nfeat
-	  		model[i] = model[i] - row[i] * Z
-	  	end
-  	end
+		sum_term = sum_term + label*rows[ir][colid]
+		pat_term = pat_term + rows[ir][colid]*1.0/(1.0+exp(-d))
+	end
+
+	model[colid] = model[colid] - 0.00001* (-sum_term + pat_term)
+	
 	return 1.0
 end
 
@@ -80,13 +88,13 @@ end
 dw = DimmWitted.open(examples, model, 
                 DimmWitted.MR_PERMACHINE,    
                 DimmWitted.DR_SHARDING,      
-                DimmWitted.AC_ROW)
+                DimmWitted.AC_C2R)
 
 ######################################
 # Register functions.
 #
 handle_loss = DimmWitted.register_row(dw, loss)
-handle_grad = DimmWitted.register_row(dw, grad)
+handle_grad = DimmWitted.register_c2r(dw, grad)
 DimmWitted.register_model_avg(dw, handle_loss, avg, true)
 DimmWitted.register_model_avg(dw, handle_grad, avg, true)
 
@@ -96,6 +104,7 @@ DimmWitted.register_model_avg(dw, handle_grad, avg, true)
 for iepoch = 1:10
 	rs = DimmWitted.exec(dw, handle_loss)
 	println("LOSS: ", rs/nexp)
+	println(sum(model))
 	rs = DimmWitted.exec(dw, handle_grad)
 end
 
