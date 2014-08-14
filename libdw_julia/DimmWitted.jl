ae195344583d15a52e9d122551a48cd5475c56c1
Ce Zhang <czhang@Ces-MacBook-Pro.local>
2014-08-14 14:14:54 -0700

    julia

---
+++ libdw_julia/DimmWitted.jl
@@ -0,0 +1,181 @@
+
+module DimmWitted
+
+immutable DW
+  _dw::Ptr{Void}
+  modelrepl::Cint
+  datarepl::Cint
+  accessmethod::Cint
+end
+
+_libpath = ""
+_data_type = ""
+_model_type = ""
+
+_nogc = {0.0,0.0}
+
+MR_PERCORE = 0
+MR_PERNODE = 1
+MR_PERMACHINE = 2
+MR_SINGLETHREAD_DEBUG = 3
+
+DR_FULL = 0
+DR_SHARDING = 1
+
+AC_ROW = 0
+AC_COL = 1
+AC_C2R = 2
+
+function set_libpath(path)
+	global _libpath
+	_libpath = path
+end   
+
+function get_libpath()
+	global _libpath
+	return _libpath
+end
+
+function open{DATATYPE, MODELTYPE}(examples::Array{DATATYPE,2}, model::Array{MODELTYPE,1}, _modelrepl, _datarepl, _acmethod)
+
+	modelrepl = convert(Cint, _modelrepl)
+	datarepl = convert(Cint, _datarepl)
+	acmethod = convert(Cint, _acmethod)
+
+	global _libpath, _dw, _data_type, _model_type, _nogc
+
+	_examples = examples
+	_model = model
+	_examples_c = examples.'
+
+	append!(_nogc, {_examples, _model, _examples_c})
+
+	nrows = size(examples, 1)
+	ncols = size(examples, 2)
+	nmodelel = size(model, 1)
+
+	_data_type = DATATYPE
+	_model_type = MODELTYPE
+
+	_dw = @eval ccall( ($(string("DenseDimmWitted_Open2")), $(_libpath)), Ptr{Void}, (Any, Any, Clonglong, Clonglong, Clonglong, Ptr{Void}, Ptr{Void}, Cint, Cint, Cint), $(Array{DATATYPE}), $(Array{MODELTYPE}), $(nrows), $(ncols), $(nmodelel), $(_examples_c), $(_model), $(modelrepl), $(datarepl), $(acmethod))
+
+	dw = DW(_dw, modelrepl, datarepl, acmethod)
+
+	println("[JULIA-DW] Created DimmWitted Object: ", dw._dw)
+
+	return dw
+end
+
+function check_is_safe(func, ret, parameter)
+
+	const stdout = STDOUT
+	const rd, wr = redirect_stdout()
+	code_llvm(func, parameter)
+	str = readavailable(rd)
+	close(rd)
+	redirect_stdout(STDERR)
+
+	if contains(str, "alloc") || contains(replace(str, string("julia_",func), ""), "julia_")
+		#println(str)
+		return false
+	else
+		return true
+	end
+end
+
+
+function register_row(_dw, func, supress=false)
+
+	is_safe = check_is_safe(func, Cdouble, (Array{_data_type,1}, Array{_model_type,1}))
+	if is_safe == false && supress==false
+		error("Your function contains LLVM LR `alloc` or `call` other julia functions. We cannot register this function because it protentially is not thread-safe. Use register_row(_dw",",",func,",true) to register this function AT YOUR OWN RISK!")
+	end
+
+	global _data_type, _model_type, _libpath, _nogc
+
+	const func_c = cfunction(func, Cdouble, (Array{_data_type,1}, Array{_model_type,1}))
+
+	append!(_nogc, {func_c, func})
+
+	handle = @eval ccall(($(string("DenseDimmWitted_Register_Row2")), $(_libpath)), Cuint, (Ptr{Void}, Ptr{Void}, Cint, Cint, Cint), $(_dw._dw), $(func_c), $(_dw.modelrepl), $(_dw.datarepl), $(_dw.accessmethod)) 
+
+	println("[JULIA-DW] Registered Row Function ", func, " Handle=", handle)
+
+	return handle
+end
+
+function register_c2r(_dw, func, supress=false)
+
+	is_safe = check_is_safe(func, Cdouble, (Array{_data_type,1}, Cint, Array{Array{Cdouble, 1},1}, Array{_model_type,1}))
+	if is_safe == false && supress==false
+		error("Your function contains LLVM LR `alloc` or `call` other julia functions. We cannot register this function because it protentially is not thread-safe. Use register_c2r(_dw",",",func,",true) to register this function AT YOUR OWN RISK!")
+	end
+
+	global _data_type, _model_type, _libpath, _nogc
+
+	const func_c = cfunction(func, Cdouble, (Array{_data_type,1}, Cint, Array{Array{Cdouble, 1},1}, Array{_model_type,1}))
+
+	append!(_nogc, {func_c, func})
+
+	handle = @eval ccall(($(string("DenseDimmWitted_Register_C2R2")), $(_libpath)), Cuint, (Ptr{Void}, Ptr{Void}, Cint, Cint, Cint), $(_dw._dw), $(func_c), $(_dw.modelrepl), $(_dw.datarepl), $(_dw.accessmethod)) 
+
+	println("[JULIA-DW] Registered Column-to-row Function ", func, " Handle=", handle)
+
+	return handle
+end
+
+function register_col(_dw, func, supress=false)
+
+	is_safe = check_is_safe(func, Cdouble, (Array{_data_type,1}, Array{_model_type,1}))
+	if is_safe == false && supress==false
+		error("Your function contains LLVM LR `alloc` or `call` other julia functions. We cannot register this function because it protentially is not thread-safe. Use register_row(_dw",",",func,",true) to register this function AT YOUR OWN RISK!")
+	end
+
+	global _data_type, _model_type, _libpath, _nogc
+
+	const func_c = cfunction(func, Cdouble, (Array{_data_type,1}, Array{_model_type,1}))
+
+	append!(_nogc, {func_c, func})
+
+	handle = @eval ccall(($(string("DenseDimmWitted_Register_Col2")), $(_libpath)), Cuint, (Ptr{Void}, Ptr{Void}, Cint, Cint, Cint), $(_dw._dw), $(func_c), $(_dw.modelrepl), $(_dw.datarepl), $(_dw.accessmethod)) 
+
+	println("[JULIA-DW] Registered Col Function ", func, " Handle=", handle)
+
+	return handle
+end
+
+function register_model_avg(_dw, handle, func, supress=false)
+
+	is_safe = check_is_safe(func, Cdouble, (Ptr{Array{_model_type,1}}, Cint, Cint))
+	if is_safe == false && supress==false
+		error("Your function contains LLVM LR `alloc` or `call` other julia functions. We cannot register this function because it protentially is not thread-safe. Use register_model_avg(_dw",",",func,",true) to register this function AT YOUR OWN RISK!")
+	end
+
+	global _data_type, _model_type, _libpath, _nogc
+
+	const func_c = cfunction(func, Cdouble, (Ptr{Array{Cdouble,1}}, Cint, Cint))
+
+	append!(_nogc, {func_c, func})
+
+	@eval ccall(($(string("DenseDimmWitted_Register_ModelAvg2")), $(_libpath)), Void, (Ptr{Void}, Cuint, Ptr{Void}, Cint, Cint, Cint), $(_dw._dw), $(handle), $(func_c), $(_dw.modelrepl), $(_dw.datarepl), $(_dw.accessmethod))
+
+	println("[JULIA-DW] Registered Avg Function ", func, " for Func Handle=", handle)
+
+	return nothing
+
+end
+
+function exec(_dw, func_handle)
+	global _libpath
+
+	rs = @eval ccall(($(string("DenseDimmWitted_Exec2")), $(_libpath)), Cdouble, (Ptr{Void}, Cuint, Cint, Cint, Cint), $(_dw._dw), $(func_handle), $(_dw.modelrepl), $(_dw.datarepl), $(_dw.accessmethod))
+
+	return rs
+end
+
+
+export libpath, get_libpath, open, register_row, exec
+
+end
+
+
