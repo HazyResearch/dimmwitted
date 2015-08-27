06fa775ee6cb8a9a724de167f4a0f54743cf90de
Christopher De Sa <cdesa@stanford.edu>
2015-08-27 16:25:58 -0700

    sync

--- src/test.cpp
+++ src/test.cpp
@@ -4,5 +4,5 @@
 
 using namespace lpblas;
 
-template dot_dense<LPBLAS_i8>(const LPBLAS_i8 * const x, const LPBLAS_i8 * const y, int N);
-template dot_dense<LPBLAS_i16>(const LPBLAS_i16 * const x, const LPBLAS_i16 * const y, int N);
+template float dot_dense<LPBLAS_i8>(const LPBLAS_i8 * const x, const LPBLAS_i8 * const y, int N);
+template float dot_dense<LPBLAS_i16>(const LPBLAS_i16 * const x, const LPBLAS_i16 * const y, int N);
